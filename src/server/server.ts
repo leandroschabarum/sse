73e6c539aab708f311a9874e199c2b228caf9513
Leandro Schabarum <leandroschabarum.98@gmail.com>
2026-02-01 14:46:46 -0300

    fix: handle extra header cases

--- src/server/server.ts
+++ src/server/server.ts
@@ -22,7 +22,9 @@ export class Server<TReq extends Request, TRes extends Response> {
 	}
 
 	protected generateId(req: TReq): string {
-		return req.headers[HEADER_REQUEST_ID]?.trim() || uuidv4();
+		const data = req.headers[HEADER_REQUEST_ID];
+
+		return (typeof data === 'string' && data?.trim()) || uuidv4();
 	}
 
 	public broadcast<T>(event: string, data?: T): void {
